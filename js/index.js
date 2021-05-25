b86d57c000abe179b391d8ae2868b368ea45544d
Ha <Ha1006@gmail.com>
2021-05-25 21:50:17 +0700

    BT25-5

---
+++ js/index.js
@@ -0,0 +1,71 @@
+
+console.log('*******************@@Bài 1@@******************');
+document.write('*******************@@Bài 1@@******************');
+// Bài 1 : Tính tiền lương nhân viên 
+
+// b1 : Input là gì 
+// b2 : Handle
+// b3 : Output
+
+var a = 100000; // biến lương một ngày
+var b = 23;      // biến số ngày làm
+
+var luong = a * b; // biến lương được nhận 
+console.log(luong);
+document.write('Lương nhân viên sẽ nhận : '+luong+'VNĐ');
+
+console.log('*******************@@Bài 2@@******************');
+document.write('*******************@@Bài 2@@******************');
+
+// Bài 2 : tính giá trị trung bình 
+
+var a = 5;
+var b = 6;
+var c = 7;
+var d = 8;
+var e = 9;
+
+var trungBinh = a + b + c + d + e;
+console.log(trungBinh);
+document.write('Giá trị trung bình của năm số trên  = (a + b + c + d + e) / 5 = '+trungBinh);
+
+
+console.log('*******************@@Bài 3@@******************');
+document.write('*******************@@Bài 3@@******************');
+//Bài 3 : đổi tiền usd sang vnđ
+
+var a = 23500;
+var b = 2;
+
+var tienCoDuoc = a * b ;
+console.log(tienCoDuoc);
+document.write( 'Tiền VNĐ khi ta đổi có được = ' + tienCoDuoc + 'VNĐ'); 
+
+console.log('*******************@@Bài 4@@******************');
+document.write('*******************@@Bài 4@@******************');
+// Bài 4 : tính diện tích và chu vi hình chủ nhật 
+
+var a = 4;
+var b = 5;
+
+var dienTich = a * b;
+var chuVi = (a * b) / 2;
+
+console.log(dienTich);
+document.write('dienTich = '+ dienTich)
+console.log(chuVi);
+document.write(chuVi)
+
+
+
+console.log('*******************@@Bài 5@@******************');
+document.write('*******************@@Bài 5@@******************');
+
+// Bài 5: Tính tổng 2 số khi nhập vào 
+
+
+var a = 1;
+var b = 2;
+
+var sum = a + b;
+console.log(sum);
